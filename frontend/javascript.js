80ec6e837af894dfa77de50800964ee2bca83dcd
MathF <mathias.fillon@gmail.com>
2017-03-17 13:21:48 +0100

    first commit

---
+++ frontend/javascript.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+    document.getElementById('supprimer').onclick = function () {
+        alert('je n`ai pas réussis cette partie');
+        
+    }
+    document.getElementById('button').onclick = function () {
+        alert('je n`ai pas réussis cette partie');
+        
+    }
+
+});
